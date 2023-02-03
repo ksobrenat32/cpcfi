7de391219a6bdde49043be11e18640c7147a7393
ksobrenat32 <ksobrenatural@tuta.io>
2023-02-02 22:06:54 -0600

    Problem: bit++

---
+++ problems/bit++.cpp
@@ -0,0 +1,36 @@
+// Codeforces link:
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FO(i, b)  for (int i = 0; i < (b); i++)
+
+inline void solve(){
+    int n;
+    cin>>n;
+    int x=0;
+
+    FO(i,n){
+        string s;
+        cin>>s;
+
+        if(s.find("++") != string::npos){
+            x++;
+        } else {
+            x--;
+        }
+    }
+
+    cout<<x<<endl;
+
+}
+
+int main(){
+    int T=1;
+    //cin>>T;
+    FO(tc,T){
+        solve();
+    }
+    return 0;
+}
