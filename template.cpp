21bddffbceff14e102ee94a2454c5b8ee1bd3930
ksobrenat32 <ksobrenat32@code-server>
2023-02-26 13:30:02 -0600

    Setup templates, mine and cpcfi

---
+++ template.cpp
@@ -0,0 +1,20 @@
+// Codeforces link:
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FO(i, b)  for (int i = 0; i < (b); i++)
+
+inline void solve(){
+
+}
+
+int main(){
+    int T=1;
+    //cin>>T;
+    FO(tc,T){
+        solve();
+    }
+    return 0;
+}
