423881b01d007e28865e9d3a126528ad233b86ef
ksobrenat32 <ksobrenatural@tuta.io>
2023-02-27 19:32:19 -0600

    Problem: 25-a

---
+++ problems/codeforces/25-a.cpp
@@ -0,0 +1,52 @@
+// Problem link:
+// https://codeforces.com/problemset/problem/25/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FO(i, b)  for (int i = 0; i < (b); i++)
+
+inline void solve(){
+    short int n;
+    cin >> n;
+
+    // Save
+    int x[n];
+    int p;
+    bool even;
+
+    // Read
+    FO(i,n){
+        cin >> x[i];
+    }
+
+    // Check the condition
+    if( ( x[0]%2 == 0 && x[1]%2 == 0 ) || ( x[1]%2 == 0 && x[2]%2 == 0) || ( x[0]%2 == 0 && x[2]%2 == 0) ){
+        even=true;
+    } else {
+        even=false;
+    }
+
+    FO(i,n){
+        if(even){
+            if( x[i]%2 != 0){
+                p = i;
+            }
+        } else {
+            if( x[i]%2 == 0){
+                p = i;
+            }
+        }
+    }
+    
+    cout << p+1 << endl;
+}
+
+int main(){
+    int T=1;
+    //cin>>T;
+    FO(tc,T){
+        solve();
+    }
+    return 0;
+}
