fa1f9a988c8c22f5c96c207c2cf54363e162d848
ksobrenat32 <ksobrenatural@tuta.io>
2023-06-25 13:53:17 -0600

    Making the template better

--- template.cpp
+++ template.cpp
@@ -1,10 +1,28 @@
-// Problem link:
-//
-
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FO(i, b)  for (int i = 0; i < (b); i++)
+#define FO(i, b)                    for (int i = 0; i < (b); i++)
+#define FOR(i, a, b)                for (int i = (a); i < (b); i++)
+#define rFOR(i, a, b)               for (int i = (a); i > (b); i--)
+#define TR(v, arr)                  for (auto& (v) : (arr))
+#define pb                          push_back
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+#define all(x)                      x.begin(), x.end() 
+#define sz(x)                       (int) x.size()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// cout << setprecision(11);
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+// Problem link:
+//
 
 inline void solve(){
 
