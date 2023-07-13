cfc8b0535070d261c64460917c7e1525e7563ff7
ksobrenat32 <ksobrenatural@tuta.io>
2023-07-12 21:30:50 -0600

    Problems: codeforces

---
+++ problems/codeforces/443-a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+#define endl                        '\n'
+#define FO(i, b)                    for (int i = 0; i < (b); i++)
+#define FOR(i, a, b)                for (int i = (a); i < (b); i++)
+#define rFOR(i, a, b)               for (int i = (a); i > (b); i--)
+#define TR(v, arr)                  for (auto& v : (arr))
+#define pb                          push_back
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+#define all(x)                      x.begin(), x.end() 
+#define sz(x)                       (int) x.size()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// cout << setprecision(11);
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+// Problem link:
+// https://codeforces.com/problemset/problem/443/A
+
+inline void solve(){
+    string s;
+    getline(cin,s);
+
+    set<int> st;
+
+    FOR(i,1,sz(s)-1){
+        st.insert(s[i]);
+        i += 2;
+    }
+
+    cout << sz(st) << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    //cin>>T;
+    FO(tc,T){
+        solve();
+    }
+    return 0;
+}
