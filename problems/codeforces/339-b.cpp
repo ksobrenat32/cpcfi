68738f1d8e045ad1799030f1ca4929a50d5262c2
ksobrenat32 <ksobrenatural@tuta.io>
2023-11-06 16:38:47 -0600

    Codeforces: Problems

---
+++ problems/codeforces/339-b.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl                        '\n'
+#define FO(i, b)                    for (int i = 0; i < (b); i++)
+#define FOR(i, a, b)                for (int i = (a); i < (b); i++)
+#define rFOR(i, a, b)               for (int i = (a); i > (b); i--)
+#define TR(v, arr)                  for (auto& v : (arr))
+#define pb                          push_back
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+#define all(x)                      x.begin(), x.end() 
+#define sz(x)                       (int) x.size()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// cout << setprecision(11);
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+// Problem link:
+// https://codeforces.com/problemset/problem/339/B
+
+inline void solve(){
+    int n, m;
+    cin >> n >> m;
+
+    vi a(m);
+    ll cnt = 0;
+
+    cin >> a[0];
+    cnt += a[0]-1;
+
+    FOR(i, 1, m){
+        cin >> a[i];
+        if(a[i-1] < a[i]){
+            cnt += a[i] - a[i-1];
+        } else if (a[i-1] > a[i]){
+            cnt += (n-a[i-1]) + a[i];
+        }
+    }
+
+    cout << cnt << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    //cin>>T;
+    FO(tc,T){
+        solve();
+    }
+    return 0;
+}
---
+++ problems/codeforces/723-a.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl                        '\n'
+#define FO(i, b)                    for (int i = 0; i < (b); i++)
+#define FOR(i, a, b)                for (int i = (a); i < (b); i++)
+#define rFOR(i, a, b)               for (int i = (a); i > (b); i--)
+#define TR(v, arr)                  for (auto& v : (arr))
+#define pb                          push_back
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+#define all(x)                      x.begin(), x.end() 
+#define sz(x)                       (int) x.size()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// cout << setprecision(11);
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+// Problem link:
+// https://codeforces.com/problemset/problem/723/A
+
+inline void solve(){
+    vi x(3,0);
+    cin >> x[0] >> x[1] >> x[2];
+
+    int mn = 1e6;
+    int cnt;
+
+    cnt = 0;
+    cnt += abs(x[0] - x[1]);
+    cnt += abs(x[0] - x[2]);
+    mn = min(mn, cnt);
+
+    cnt = 0;
+    cnt += abs(x[1] - x[0]);
+    cnt += abs(x[1] - x[2]);
+    mn = min(mn, cnt);
+
+    cnt = 0;
+    cnt += abs(x[2] - x[0]);
+    cnt += abs(x[2] - x[1]);
+    mn = min(mn, cnt);
+
+    cout << mn << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    //cin>>T;
+    FO(tc,T){
+        solve();
+    }
+    return 0;
+}
