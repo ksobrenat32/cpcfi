e5e00c80ba03df0a31a637284b5f54e69b934914
ksobrenat32 <ksobrenatural@tuta.io>
2024-03-02 11:41:09 -0600

    Nos papearon ):

---
+++ contests/codeforces/CPCFI-5G-1/C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl                        '\n'
+#define FO(i, b)                    for (int i = 0; i < (b); i++)
+#define FOR(i, a, b)                for (int i = (a); i < (b); i++)
+#define rFOR(i, a, b)               for (int i = (a); i > (b); i--)
+#define TR(v, arr)                  for (auto& v : (arr))
+#define pb                          push_back
+#define mp                          make_pair
+#define F                           first
+#define S                           second
+#define all(x)                      x.begin(), x.end() 
+#define sz(x)                       (int) x.size()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// cout << setprecision(11);
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+// Problem link:
+// https://codeforces.com/group/mOycVt0fIB/contest/507820/problem/C
+
+inline void solve(){
+    int n, m;
+    cin >> n >> m;
+
+    ll r = m * (m+1) / 2;
+
+    for(ll i = 2; i <= n; i++){
+        r += i * m;
+    }
+
+    cout << r << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T=1;
+    cin>>T;
+    FO(tc,T){
+        solve();
+    }
+    return 0;
+}
